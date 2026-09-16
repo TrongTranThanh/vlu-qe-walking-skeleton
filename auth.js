fed2a60ba2245c1f6deca3d700c1c15666b5b382
Trần Thanh Trọng <trong.2474802010408@vanlanguni.vn>
2026-09-16 16:06:53 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@
  * @returns {boolean} - Trả về true nếu hợp lệ, ngược lại false
  */
 function login(username, password) {
-    if (username === 'admin' && password === '123') {
+    if (username === 'admin' && password === '1234') {
         return true;
     }
     return false;
